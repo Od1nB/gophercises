8f468eed66977d5c23eba2342b2bd3f94ab0c42e
Odin <odinbjoernebo@gmail.com>
2022-11-24 09:56:29 +0100

    mahandler implemented

--- ex2_urlshort/handler.go
+++ ex2_urlshort/handler.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -11,8 +12,15 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	fmt.Println("Maphandler:")
+	return func(w http.ResponseWriter, r *http.Request) {
+		if path, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, path, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
