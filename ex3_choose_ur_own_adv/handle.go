7c4affcb713246f50b7c7a130db4e8be0e18f4e4
Odin Bjørnebo <odinbjoernebo@gmail.com>
2022-11-28 15:33:00 +0100

    basic routing and templates implemented

--- ex3_choose_ur_own_adv/handle.go
+++ ex3_choose_ur_own_adv/handle.go
@@ -1,9 +1,64 @@
-package handle
+package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
+	"net/http"
 	"os"
+	"strings"
 )
+
+func init() {
+	templ = template.Must(template.New("").Parse(defaulTemp))
+}
+
+var templ *template.Template
+
+var defaulTemp = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+    {{range .Options}}
+        <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+    {{end}}
+    </ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	path = path[1:]
+
+	if _, ok := h.s[path]; ok {
+		err := templ.Execute(w, h.s[path])
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 type Story map[string]Arc
 
@@ -18,9 +73,9 @@ type Option struct {
 	Arc  string `json:"arc"`
 }
 
-func ParseStory() (Story, error) {
+func ParseStory(filename string) (Story, error) {
 	var story Story
-	f, err := os.Open("gopher.json")
+	f, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
--- ex3_choose_ur_own_adv/main/main.go
+++ ex3_choose_ur_own_adv/main/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	handler "cyoa"
+	cyoa "cyoa"
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	filename := flag.String("filename", "gopher.json", "a json file for your cyoa")
+	flag.Parse()
 
 	fmt.Println("new story")
-	_, err := handler.ParseStory()
+	story, err := cyoa.ParseStory(*filename)
 	if err != nil {
 		fmt.Println(err)
 	}
+	handle := cyoa.NewHandler(story)
 
+	fmt.Println("Starting new server")
+	log.Fatal(http.ListenAndServe(":3000", handle))
 }
