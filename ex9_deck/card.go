d9eaa52bc9a9a728fb0a3d23fad6ad89d3d425d9
Odin Bjørnebo <odinbjoernebo@gmail.com>
2023-03-12 20:33:01 +0100

    jokers function

--- ex9_deck/card.go
+++ ex9_deck/card.go
@@ -108,3 +108,15 @@ func Shuffle(cards []Card) []Card {
 	}
 	return outCards
 }
+
+func Jokers(n int) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{
+				Rank: Rank(i),
+				Suit: Joker,
+			})
+		}
+		return cards
+	}
+}
